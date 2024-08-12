dac7305098a7f8b6c4313bf1dfc31005ef582d2d
Alves0611 <g.alves0611@gmail.com>
2024-08-12 11:55:37 -0300

    feat: create VPC endpoints for S3 (Gateway) and CloudWatch Logs (Interface)

--- terraform/01-network/modules/vpc_endpoint/main.tf
+++ terraform/01-network/modules/vpc_endpoint/main.tf
@@ -10,3 +10,19 @@ resource "aws_security_group" "vpce" {
     cidr_blocks = [var.cidr_block]
   }
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = var.private_route_table_ids
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = var.vpc_id
+  private_dns_enabled = true
+  service_name        = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.vpce.id]
+  subnet_ids          = var.private_subnet_ids
+}
