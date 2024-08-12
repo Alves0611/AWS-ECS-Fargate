0d6031fee0d571e5b28c333fc573d8ad42ec3500
Alves0611 <g.alves0611@gmail.com>
2024-08-12 11:37:34 -0300

    chore: add locals for determining NAT Gateway and NAT Instance usage

--- terraform/01-network/locals.tf
+++ terraform/01-network/locals.tf
@@ -1,6 +1,8 @@
 locals {
   namespaced_department_name = "${var.department_name}-${var.environment}"
 
+  use_nat_gateway      = var.use_nat_gateway
+  use_nat_instance     = var.use_nat_instance && local.use_nat_gateway == false
   enable_dns_support   = (var.use_nat_instance || var.create_vpc_endpoint) ? true : var.network.enable_dns_support
   enable_dns_hostnames = (var.use_nat_instance || var.create_vpc_endpoint) ? true : var.network.enable_dns_hostnames
 
