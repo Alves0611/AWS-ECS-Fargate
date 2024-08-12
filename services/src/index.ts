9514c3842747158c4dfd708fadd04ee2af59e5d5
Alves0611 <g.alves0611@gmail.com>
2024-08-12 08:56:12 -0300

    feat: add basic routes for root and health check endpoints

--- services/src/index.ts
+++ services/src/index.ts
@@ -1,10 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { createServer } from 'http';
 
 const app = express();
-const server = createServer(app);
-const port = 8080;
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+})
+
+app.get("/healthcheck", (req: Request, res: Response) => {
+  try {
+    res.status(200).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+const server = createServer(app);
+
+const port = 8080;
 server.listen(8080, () => {
   console.log(`Server is running on port ${port}`)
 })
