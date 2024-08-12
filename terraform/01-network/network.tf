53c630a5203fea6357e21dd358cb9722620dfd8f
Alves0611 <g.alves0611@gmail.com>
2024-08-12 11:42:00 -0300

    feat: create public and private subnets across selected availability zones

--- terraform/01-network/network.tf
+++ terraform/01-network/network.tf
@@ -15,3 +15,26 @@ resource "aws_internet_gateway" "this" {
     "Name" = local.namespaced_department_name
   }
 }
+
+resource "aws_subnet" "private" {
+  count = var.network.az_count
+
+  availability_zone = local.selected_availability_zones[count.index]
+  vpc_id            = aws_vpc.this.id
+
+  tags = {
+    "Name" = "${local.namespaced_department_name}-private-${local.selected_availability_zones[count.index]}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = var.network.az_count
+
+  cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, 8, var.network.az_count + count.index)
+  availability_zone = local.selected_availability_zones[count.index]
+  vpc_id            = aws_vpc.this.id
+
+  tags = {
+    "Name" = "${local.namespaced_department_name}-public-${local.selected_availability_zones[count.index]}"
+  }
+}
