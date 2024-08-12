5c345bfd8dd2058470a5f24e070784572e2285d4
Alves0611 <g.alves0611@gmail.com>
2024-08-12 11:41:32 -0300

    chore: add locals for selecting and sorting AWS availability zones

--- terraform/01-network/locals.tf
+++ terraform/01-network/locals.tf
@@ -6,6 +6,9 @@ locals {
   enable_dns_support   = (var.use_nat_instance || var.create_vpc_endpoint) ? true : var.network.enable_dns_support
   enable_dns_hostnames = (var.use_nat_instance || var.create_vpc_endpoint) ? true : var.network.enable_dns_hostnames
 
+  sorted_availability_zones   = sort(data.aws_availability_zones.all.names)
+  selected_availability_zones = slice(local.sorted_availability_zones, 0, var.network.az_count)
+
   common_tags = {
     Component = "Network"
     ManagedBy = "Terraform"
