db9f4e8ca661c57cc4bc09aaef148c3eb96192f4
Alves0611 <g.alves0611@gmail.com>
2024-08-12 08:59:01 -0300

    feat: add CRUD routes for todos in API v1

--- services/src/index.ts
+++ services/src/index.ts
@@ -15,6 +15,26 @@ app.get("/healthcheck", (req: Request, res: Response) => {
   }
 });
 
+app.get("/api/v1/todos", (req: Request, res: Response) => {
+  res.send("GET TODOS");
+});
+
+app.get("/api/v1/todos/:id", (req: Request, res: Response) => {
+  res.send("GET TODO BY ID");
+});
+
+app.post("/api/v1/todos", (req: Request, res: Response) => {
+  res.send("POST CREATE TODO");
+});
+
+app.patch("/api/v1/todos/:id", (req: Request, res: Response) => {
+  res.send("UPDATE TODO BY ID");
+});
+
+app.delete("/api/v1/todos/:id", (req: Request, res: Response) => {
+  res.send("DELETE TODO BY ID");
+});
+
 const server = createServer(app);
 
 const port = 8080;
