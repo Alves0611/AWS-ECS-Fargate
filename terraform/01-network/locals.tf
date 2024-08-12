6a26ec4a04973ed88a25c357fcad370feaba166c
Alves0611 <g.alves0611@gmail.com>
2024-08-12 11:35:58 -0300

    chore: add calculated locals for namespaced department name and DNS settings

--- terraform/01-network/locals.tf
+++ terraform/01-network/locals.tf
@@ -1,7 +1,12 @@
 locals {
+  namespaced_department_name = "${var.department_name}-${var.environment}"
+
+  enable_dns_support   = (var.use_nat_instance || var.create_vpc_endpoint) ? true : var.network.enable_dns_support
+  enable_dns_hostnames = (var.use_nat_instance || var.create_vpc_endpoint) ? true : var.network.enable_dns_hostnames
+
   common_tags = {
-    Component  = "Network"
-    ManagedBy  = "Terraform"
-    Env        = var.environment
+    Component = "Network"
+    ManagedBy = "Terraform"
+    Env       = var.environment
   }
 }
