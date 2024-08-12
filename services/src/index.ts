02456284578560ccc92234cd56ba7cc493a18ebd
Alves0611 <g.alves0611@gmail.com>
2024-08-12 09:58:19 -0300

    feat: add global error handling for unhandled rejections and uncaught exceptions

--- services/src/index.ts
+++ services/src/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import { createServer } from 'http';
 
 import { db, todos } from "./db";
+import { logger as pino, errorHandler } from "./utils";
 
 const app = express();
 
@@ -45,9 +46,29 @@ app.delete("/api/v1/todos/:id", (req: Request, res: Response) => {
   res.send("DELETE TODO BY ID");
 });
 
+app.use((err: any, req: Request, res: Response, next: any) => {
+  errorHandler.handleError(err, res);
+});
+
 const server = createServer(app);
 
 const port = process.env.PORT ?? 8081;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
+
+process.on("unhandledRejection", (reason, promise) => {
+  pino.error({ promise, reason }, "Unhandled Rejection");
+});
+
+process.on("uncaughtException", (error) => {
+  pino.error({ error }, "Uncaught Exception");
+
+  server.close(() => {
+    pino.info("Server closed");
+    process.exit(1);
+  });
+
+  // If the server hasn't finished in a reasonable time, give it 10 seconds and force exit
+  setTimeout(() => process.exit(1), 10000).unref();
+});
