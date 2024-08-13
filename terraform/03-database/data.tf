898682de35cfaa6e1a9f23bebbc9a7a23d2122d1
Alves0611 <g.alves0611@gmail.com>
2024-08-13 18:14:37 -0300

    feat: add Terraform remote state configuration for network

--- terraform/03-database/data.tf
+++ terraform/03-database/data.tf
@@ -1,4 +1,13 @@
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "all" {}
 
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-891377404175"
+    key    = "ecs-fargate/dev/network/terraform.tfstate"
+    region = var.aws_region
+  }
+}
 
