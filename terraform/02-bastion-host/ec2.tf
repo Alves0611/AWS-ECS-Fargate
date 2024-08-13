a09c1ce2d8dc3145ea8bc209fb96d341c686e0f2
Alves0611 <g.alves0611@gmail.com>
2024-08-13 14:13:41 -0300

    feat: add EC2 instance resource with public IP association and security group

--- terraform/02-bastion-host/ec2.tf
+++ terraform/02-bastion-host/ec2.tf
@@ -22,3 +22,15 @@ resource "aws_security_group" "this" {
   }
 }
 
+resource "aws_instance" "this" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = local.subnets.public.id[0]
+  vpc_security_group_ids      = [aws_security_group.this.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = local.namespaced_service_name
+  }
+}
