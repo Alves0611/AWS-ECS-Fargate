d5bd56a9afcde88da94a9cae04d47d0bd521d568
Alves0611 <g.alves0611@gmail.com>
2024-08-13 13:53:01 -0300

    feat: add EC2 instance configuration for NAT with associated security group, IAM profile, and provisioning scripts

--- terraform/01-network/modules/nat_instance/main.tf
+++ terraform/01-network/modules/nat_instance/main.tf
@@ -47,3 +47,29 @@ resource "aws_security_group" "this" {
   }
 }
 
+resource "aws_instance" "this" {
+  count = var.nat_instance_count
+
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = var.nat_instance_type
+  subnet_id                   = var.public_subnet_ids[count.index]
+  associate_public_ip_address = "true"
+  source_dest_check           = "false"
+  vpc_security_group_ids      = [aws_security_group.this.id]
+  iam_instance_profile        = aws_iam_instance_profile.this.id
+  user_data                   = file("${path.module}/scripts/user_data.sh")
+
+  tags = {
+    Name = "${var.vpc_name}-az-${local.az_codes[count.index]}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # Script to give NAT Instance time to wake up
+  provisioner "local-exec" {
+    command = "./${path.module}/scripts/check_instance_state.sh ${self.id}"
+  }
+}
+
