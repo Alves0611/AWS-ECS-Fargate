945848f291d2cda496964ffe6aec2b98dfb4485b
Alves0611 <g.alves0611@gmail.com>
2024-08-13 14:15:22 -0300

    feat: add input variables for allowed IP, EC2 instance type, and SSH key name

--- terraform/02-bastion-host/variables.tf
+++ terraform/02-bastion-host/variables.tf
@@ -16,4 +16,18 @@ variable "service_name" {
   default     = "express-todo-api"
 }
 
+variable "allowed_ip" {
+  description = "IP address/CIDR block allowed to SSH into the bastion host. Use caution to restrict access to known IPs for security"
+  type        = string
+}
 
+variable "instance_type" {
+  description = "The EC2 instance type for the bastion host, such as 't2.micro'. This determines the hardware of the host"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair used to access the bastion host. Ensure the key exists in AWS EC2 before deployment"
+  type        = string
+}
