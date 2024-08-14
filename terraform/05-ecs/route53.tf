60a8106e96bb14d0dbbdf62c5ace67103af66d3d
Alves0611 <g.alves0611@gmail.com>
2024-08-14 14:15:19 -0300

    feat: add Route 53 records for ACM certificate validation

--- terraform/05-ecs/route53.tf
+++ terraform/05-ecs/route53.tf
@@ -18,3 +18,20 @@ resource "aws_route53_record" "api" {
     zone_id                = aws_alb.this.zone_id
   }
 }
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = local.has_domain_name ? {
+    for dvo in aws_acm_certificate.this[0].domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  } : {}
+
+  allow_overwrite = true
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  zone_id         = data.aws_route53_zone.this[0].zone_id
+  ttl             = 60
+}
