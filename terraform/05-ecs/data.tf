004ff8968d59bbf4eab861bf4dd125bf9c2f2d31
Alves0611 <g.alves0611@gmail.com>
2024-08-14 12:29:05 -0300

    feat: add data sources for AWS availability zones and remote state for network, ECR, and database

--- terraform/05-ecs/data.tf
+++ terraform/05-ecs/data.tf
@@ -1,1 +1,34 @@
 data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "all" {}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-891377404175"
+    key    = "ecs-fargate/${var.environment}/network/terraform.tfstate"
+    region = var.aws_region
+  }
+}
+
+data "terraform_remote_state" "ecr" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-891377404175"
+    key    = "ecs-fargate/${var.environment}/ecr/terraform.tfstate"
+    region = var.aws_region
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-891377404175"
+    key    = "ecs-fargate/${var.environment}/database/terraform.tfstate"
+    region = var.aws_region
+  }
+}
+
+
