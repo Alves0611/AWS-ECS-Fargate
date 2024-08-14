a6bb322255db4faecccc05db57e0e5f635373955
Alves0611 <g.alves0611@gmail.com>
2024-08-14 12:43:12 -0300

    feat: add IAM role policy attachment for ECS task execution and create IAM role for ECS tasks

--- terraform/05-ecs/iam.tf
+++ terraform/05-ecs/iam.tf
@@ -19,3 +19,15 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
 
+# Includes permissions like pulling container images from ECR and writing logs to CloudWatch.
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ecs_task_role
+# This role is necessary for the tasks themselves to perform actions on AWS services
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "${var.service_name}-task-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
