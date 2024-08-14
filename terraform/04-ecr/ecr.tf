ed4e1307518e914e181a5c12df07da5d377371f0
Alves0611 <g.alves0611@gmail.com>
2024-08-14 11:37:14 -0300

    feat: add ECR lifecycle policy to retain last 5 images

--- terraform/04-ecr/ecr.tf
+++ terraform/04-ecr/ecr.tf
@@ -4,3 +4,22 @@ resource "aws_ecr_repository" "this" {
   force_delete         = var.force_delete_repo
 }
 
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 5 images"
+      action = {
+        type = "expire"
+      }
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 5
+      }
+    }]
+  })
+
+}
