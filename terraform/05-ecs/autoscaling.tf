89187f9d8b65ebb6a856651deaefbcf5074e3bcb
Alves0611 <g.alves0611@gmail.com>
2024-08-14 14:12:10 -0300

    feat: add auto-scaling policies for ECS service based on memory and CPU utilization

--- terraform/05-ecs/autoscaling.tf
+++ terraform/05-ecs/autoscaling.tf
@@ -6,4 +6,39 @@ resource "aws_appautoscaling_target" "this" {
   service_namespace  = "ecs"
 }
 
+# https://docs.aws.amazon.com/autoscaling/application/userguide/application-auto-scaling-target-tracking.html
+resource "aws_appautoscaling_policy" "ecs_policy_memory" {
+  name               = "memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.this.resource_id
+  scalable_dimension = aws_appautoscaling_target.this.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.this.service_namespace
 
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = var.autoscaling.memory_autoscaling.target
+    scale_in_cooldown  = var.autoscaling.memory_autoscaling.scale_in_cooldown
+    scale_out_cooldown = var.autoscaling.memory_autoscaling.scale_out_cooldown
+  }
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  name               = "cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.this.resource_id
+  scalable_dimension = aws_appautoscaling_target.this.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.this.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = var.autoscaling.cpu_autoscaling.target
+    scale_in_cooldown  = var.autoscaling.cpu_autoscaling.scale_in_cooldown
+    scale_out_cooldown = var.autoscaling.cpu_autoscaling.scale_out_cooldown
+  }
+}
