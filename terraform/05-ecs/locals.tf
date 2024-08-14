e7ed7e516771caf481e67d7f79880e0febd231e7
Alves0611 <g.alves0611@gmail.com>
2024-08-14 13:01:43 -0300

    feat: add local variables for ECS container name, app image, and autoscaling resource ID

--- terraform/05-ecs/locals.tf
+++ terraform/05-ecs/locals.tf
@@ -3,6 +3,11 @@ locals {
   namespaced_service_name    = "${var.service_name}-${var.environment}"
   account_id                 = data.aws_caller_identity.current.account_id
 
+  # ECS
+  container_name          = "${local.namespaced_service_name}-container"
+  app_image               = var.ecs.app_image != "" ? var.ecs.app_image : "${data.terraform_remote_state.ecr.outputs.repository_url}:${data.terraform_remote_state.ecr.outputs.version}"
+  autoscaling_resource_id = "service/${aws_ecs_cluster.this.name}/${aws_ecs_service.this.name}"
+
 
   # Network
   vpc     = data.terraform_remote_state.network.outputs.vpc
