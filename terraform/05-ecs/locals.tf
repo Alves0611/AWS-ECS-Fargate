93098ad5ed0162f1c158586c885325174ff58cb4
Alves0611 <g.alves0611@gmail.com>
2024-08-14 13:01:09 -0300

    feat: add local variables for VPC and subnets from remote state

--- terraform/05-ecs/locals.tf
+++ terraform/05-ecs/locals.tf
@@ -3,6 +3,12 @@ locals {
   namespaced_service_name    = "${var.service_name}-${var.environment}"
   account_id                 = data.aws_caller_identity.current.account_id
 
+
+  # Network
+  vpc     = data.terraform_remote_state.network.outputs.vpc
+  subnets = data.terraform_remote_state.network.outputs.subnets
+
+
   common_tags = {
     Component = "ECS Fargate"
     ManagedBy = "Terraform"
