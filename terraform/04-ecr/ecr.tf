969594acf3fd45457bfa4a7c3877588e61c56acf
Alves0611 <g.alves0611@gmail.com>
2024-08-14 11:37:49 -0300

    feat: add terraform_data resource for building and pushing Docker image to ECR

--- terraform/04-ecr/ecr.tf
+++ terraform/04-ecr/ecr.tf
@@ -23,3 +23,35 @@ resource "aws_ecr_lifecycle_policy" "this" {
   })
 
 }
+
+resource "terraform_data" "build_image" {
+  triggers_replace = {
+    hash = local.service_file_hash
+  }
+
+  provisioner "local-exec" {
+    working_dir = var.app_folder
+    command     = <<EOT
+      aws ecr get-login-password \
+        --region ${var.aws_region} \
+      | docker login \
+        --username AWS \
+        --password-stdin ${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
+    EOT
+  }
+
+  provisioner "local-exec" {
+    working_dir = var.app_folder
+    command     = "docker build -t ${local.namespaced_service_name} ."
+  }
+
+  provisioner "local-exec" {
+    working_dir = var.app_folder
+    command     = "docker tag ${local.namespaced_service_name} ${aws_ecr_repository.this.repository_url}:${random_id.version.id}"
+  }
+
+  provisioner "local-exec" {
+    working_dir = var.app_folder
+    command     = "docker push ${aws_ecr_repository.this.repository_url}:${random_id.version.id}"
+  }
+}
