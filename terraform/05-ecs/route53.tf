1f0fb7af3e3d721380a6f580f14962af59ad07d9
Alves0611 <g.alves0611@gmail.com>
2024-08-14 14:14:39 -0300

    feat: create Route 53 A record for API subdomain pointing to ALB

--- terraform/05-ecs/route53.tf
+++ terraform/05-ecs/route53.tf
@@ -5,3 +5,16 @@ data "aws_route53_zone" "this" {
   private_zone = false
 }
 
+resource "aws_route53_record" "api" {
+  count = local.create_resource_based_on_domain_name
+
+  name    = local.subdomain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.this[0].zone_id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_alb.this.dns_name
+    zone_id                = aws_alb.this.zone_id
+  }
+}
