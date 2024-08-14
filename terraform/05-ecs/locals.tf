a0a448f92aa8a83e7389ab067606c66412f97ccc
Alves0611 <g.alves0611@gmail.com>
2024-08-14 12:24:31 -0300

    feat: add local variables for namespacing, account ID, and common tags

---
+++ terraform/05-ecs/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  namespaced_department_name = "${var.department_name}-${var.environment}"
+  namespaced_service_name    = "${var.service_name}-${var.environment}"
+  account_id                 = data.aws_caller_identity.current.account_id
+
+  common_tags = {
+    Component = "ECS Fargate"
+    ManagedBy = "Terraform"
+    Env       = var.environment
+  }
+}
