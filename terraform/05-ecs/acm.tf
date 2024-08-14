11873bfd25f0fc367dd05d6c828dc2c836e1b50c
Alves0611 <g.alves0611@gmail.com>
2024-08-14 14:17:40 -0300

    feat: add ACM certificate validation using Route 53 DNS records

--- terraform/05-ecs/acm.tf
+++ terraform/05-ecs/acm.tf
@@ -5,4 +5,9 @@ resource "aws_acm_certificate" "this" {
   validation_method = "DNS"
 }
 
+resource "aws_acm_certificate_validation" "this" {
+  count = local.create_resource_based_on_domain_name
 
+  certificate_arn         = aws_acm_certificate.this[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
